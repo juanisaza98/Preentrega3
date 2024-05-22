d9fa8c46a370e5926de2c5511fabfc8c65bf21d5
Juan Manuel Isaza Giraldo <isaza208@hotmail.com>
2024-05-21 21:56:28 -0500

    Secret key y docs

--- project/config/settings.py
+++ project/config/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-j%*=m_@ewkazx83-8w5(7gz(i-*_gj90^c8&(^=wkaq#ul20*_"
+from django.core.management.utils import get_random_secret_key
+# SECRET_KEY = "django-insecure-j%*=m_@ewkazx83-8w5(7gz(i-*_gj90^c8&(^=wkaq#ul20*_"
+SECRET_KEY = get_random_secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
