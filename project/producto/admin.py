a04dc88d8553d51c7e3918c7526be85b092bf7a0
Juan Manuel Isaza Giraldo <isaza208@hotmail.com>
2024-05-21 21:30:10 -0500

    Se personaliza admin

--- project/producto/admin.py
+++ project/producto/admin.py
@@ -3,6 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from . import models
 
+admin.site.site_title = "Productos y categorias"
+
 
 admin.site.register(models.ProductoCategoria)
-admin.site.register(models.Producto)
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "descripcion", "precio")
+    list_display_links = ("nombre",)
+
+admin.site.register(models.Producto, ProductoAdmin)
+
+
