04da795b4c330377a5635e17984c7950a0e2e8d7
Juan Manuel Isaza Giraldo <isaza208@hotmail.com>
2024-05-21 20:47:06 -0500

    Se cambian las funciones de producto del crud por clases

--- project/producto/urls.py
+++ project/producto/urls.py
@@ -7,8 +7,12 @@ from . import views
 urlpatterns = [
     path("home/", views.home, name="home"),
     path("categoria/create/", views.categoria_create, name="categoria_create"),
-    path("producto/create/", views.producto_create, name="producto_create"),
-    path("producto/detail/<int:pk>", views.producto_detail, name="producto_detail"),
-    path("producto/update/<int:pk>", views.producto_update, name="producto_update"),
-    path("producto/delete/<int:pk>", views.producto_delete, name="producto_delete"),
+    # path("producto/create/", views.producto_create, name="producto_create"),
+    path("producto/create/", views.ProductoCreate.as_view(), name="producto_create"),
+    # path("producto/detail/<int:pk>", views.producto_detail, name="producto_detail"),
+    path("producto/detail/<int:pk>", views.ProductoDetail.as_view(), name="producto_detail"),
+    # path("producto/update/<int:pk>", views.producto_update, name="producto_update"),
+    path("producto/update/<int:pk>", views.ProductoUpdate.as_view(), name="producto_update"),
+    # path("producto/delete/<int:pk>", views.producto_delete, name="producto_delete"),
+    path("producto/delete/<int:pk>", views.ProductoDelete.as_view(), name="producto_delete"),
 ]
--- project/producto/views.py
+++ project/producto/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
 from . import models, forms
 
@@ -13,7 +15,6 @@ def home(request):
         query = models.Producto.objects.all()
         
     consulta_categorias = models.ProductoCategoria.objects.all()
-    # consulta_productos = models.Producto.objects.all()
     context = {"categorias":consulta_categorias, "productos":query}
     return render(request, "producto/index.html", context)
 
@@ -27,34 +28,61 @@ def categoria_create(request):
         form_categoria_create = forms.ProductoCategoriaForm()
     return render(request, "producto/categoria_create.html", context={"form_categoria_create": form_categoria_create})
 
-def producto_create(request):
-    if request.method == "POST":
-        form_producto_create = forms.ProductoForm(request.POST)
-        if form_producto_create.is_valid():
-            form_producto_create.save()
-            return redirect("producto:home")
-    else:
-        form_producto_create = forms.ProductoForm()
-    return render(request, "producto/producto_create.html", context={"form_producto_create": form_producto_create})
+#Producto create en funcion
+# def producto_create(request):
+#     if request.method == "POST":
+#         form_producto_create = forms.ProductoForm(request.POST)
+#         if form_producto_create.is_valid():
+#             form_producto_create.save()
+#             return redirect("producto:home")
+#     else:
+#         form_producto_create = forms.ProductoForm()
+#     return render(request, "producto/producto_create.html", context={"form_producto_create": form_producto_create})
 
-def producto_detail(request, pk: int):
-    query = models.Producto.objects.get(id=pk)
-    return render(request, "producto/producto_detail.html", {"producto": query})
+class ProductoCreate(CreateView):
+    model = models.Producto
+    form_class = forms.ProductoForm
+    template_name = 'producto/producto_create.html'
+    success_url = reverse_lazy('producto:home')
+    
 
-def producto_update(request, pk: int):
-    query = models.Producto.objects.get(id=pk)
-    if request.method == "POST":
-        form_producto_update = forms.ProductoForm(request.POST, instance=query)
-        if form_producto_update.is_valid():
-            form_producto_update.save()
-            return redirect("producto:home")
-    else:
-        form_producto_update = forms.ProductoForm(instance=query)
-    return render(request, "producto/producto_update.html", context={"form_producto_update": form_producto_update})
+#Funcion producto detail
+# def producto_detail(request, pk: int):
+#     query = models.Producto.objects.get(id=pk)
+#     return render(request, "producto/producto_detail.html", {"producto": query})
 
-def producto_delete(request, pk: int):
-    query = models.Producto.objects.get(id=pk)
-    if request.method == "POST":
-       query.delete()
-       return redirect("producto:home")
-    return render(request, "producto/producto_delete.html", context={"producto": query})
+class ProductoDetail(DetailView):
+    model = models.Producto
+    context_object_name = 'producto'
+
+#Funcion para Update
+# def producto_update(request, pk: int):
+#     query = models.Producto.objects.get(id=pk)
+#     if request.method == "POST":
+#         form_producto_update = forms.ProductoForm(request.POST, instance=query)
+#         if form_producto_update.is_valid():
+#             form_producto_update.save()
+#             return redirect("producto:home")
+#     else:
+#         form_producto_update = forms.ProductoForm(instance=query)
+#     return render(request, "producto/producto_update.html", context={"form_producto_update": form_producto_update})
+
+class ProductoUpdate(UpdateView):
+    model = models.Producto
+    form_class = forms.ProductoForm
+    template_name = 'producto/producto_update.html'
+    success_url = reverse_lazy('producto:home')
+
+# Funcion producto Delete
+# def producto_delete(request, pk: int):
+#     query = models.Producto.objects.get(id=pk)
+#     if request.method == "POST":
+#        query.delete()
+#        return redirect("producto:home")
+#     return render(request, "producto/producto_delete.html", context={"producto": query})
+
+class ProductoDelete(DeleteView):
+    model = models.Producto
+    template_name = 'producto/producto_delete.html'
+    success_url = reverse_lazy('producto:home')
+    
