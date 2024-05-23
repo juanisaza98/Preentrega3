e4077101e9beb9722c8eb61180c03c5b660a2034
Juan Manuel Isaza Giraldo <isaza208@hotmail.com>
2024-05-22 19:25:11 -0500

    Se agrega login y logout

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,10 +1,12 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
+from . import views
 
 app_name = "core"
 
-from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    
+    path("login/", views.CustomLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(template_name = "core/logout.html"), name="logout"),
 ]
--- project/core/views.py
+++ project/core/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from .forms import CustomAuthenticationForm
 
 # Create your views here.
 from . import models
 
 def home(request):
     return render(request, "core/index.html")
+
+class CustomLoginView(LoginView):
+    authentication_form = CustomAuthenticationForm
+    template_name = "core/login.html"
